522288de6783bc77447524d6459a006c84723158
Scr1ptK1ddie <161035592+Scr1ptK1ddie@users.noreply.github.com>
2024-02-25 09:42:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ subprocess.check_call(['pip', 'install', 'discord_webhook', 'termcolor'])
 import termcolor
 from discord_webhook import DiscordWebhook
 import time
+import os
+
+logo = '''
+ __          __  _     _                 _       _____                                           
+ \ \        / / | |   | |               | |     / ____|                                          
+  \ \  /\  / /__| |__ | |__   ___   ___ | | __ | (___  _ __   __ _ _ __ ___  _ __ ___   ___ _ __ 
+   \ \/  \/ / _ \ '_ \| '_ \ / _ \ / _ \| |/ /  \___ \| '_ \ / _` | '_ ` _ \| '_ ` _ \ / _ \ '__|
+    \  /\  /  __/ |_) | | | | (_) | (_) |   <   ____) | |_) | (_| | | | | | | | | | | |  __/ |   
+     \/  \/ \___|_.__/|_| |_|\___/ \___/|_|\_\ |_____/| .__/ \__,_|_| |_| |_|_| |_| |_|\___|_|   
+                                                      | |                                        
+    By: Scr1ptK1ddie                                  |_|                                        
+    '''
 
 def send_message(webhook_url, message, times, avatar_url=None):
     try:
@@ -14,10 +26,13 @@ def send_message(webhook_url, message, times, avatar_url=None):
         print(termcolor.colored(f'[-] Error: {e}', 'red'))
 
 def main():
+    os.system("clear")
+    print(termcolor.colored(logo,  "yellow"))
     webhook_url = input(termcolor.colored("[?] Enter the webhook URL: ", 'yellow'))
     avatar_url = "https://raw.githubusercontent.com/Scr1ptK1ddie/Discord-Webhook-Spammer/main/SK.png"
     message = input(termcolor.colored("[?] Enter the message: ", 'yellow'))
     times = int(input(termcolor.colored("[?] Enter the number of times: ", 'yellow')))
+    os.system("clear")
 
     send_message(webhook_url, message, times, avatar_url)
 
